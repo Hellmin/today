4f1b1b2adf140ca7630ceabec59856bed3bea0b2
Hellmin <56633811+Hellmin@users.noreply.github.com>
2021-01-13 18:46:56 +0500

    tommorow button

--- builder.js
+++ builder.js
@@ -1,28 +1,82 @@
-//текущий месяц
-const workdays = [
-	1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31
-]
+const [job, color, gif] = varMaker(workdays);
+buildApp(job, color, gif);
 
-//текущая дата
-const today = new Date().getDate();
-
-let job = 'Не работает!';
-let color = 'green';
-let gif = 'good';
-
-//проверка текущей даты и рабочего дня
-for (workday of workdays){
-	if (today == workday) {
-		job = 'Работает!';
-		color = 'red';
-		gif = 'bad';
-		break;
-	} else if (today == ++workday) {
-		job = 'Приходит со смены';
-		color = 'blue';
-		gif = 'okay';
-		break;
+function varMaker(array, mod = 0){
+	let job = 'Не работает!';
+	let color = 'green';
+	let gif = 'good';
+	const today = new Date().getDate();
+	for (workday of array){
+		if (today + mod == workday) {
+			job = 'Работает!';
+			color = 'red';
+			gif = 'bad';
+			break;
+		} else if (today + mod == ++workday) {
+			job = 'Приходит со смены';
+			color = 'blue';
+			gif = 'okay';
+			break;
+		}
 	}
+	return [job, color, gif];
+}
+
+function buildApp(job, color, gif, hidden = true, text='ХЕЛЬ СЕГОДНЯ...'){
+	const isHidden = hidden ? 'hidden ' : '';
+
+	//блок с датой
+	const dateBlock = createElement('div', '', {
+		'class' : 'wrapper date-block'
+	})
+	const currentDay = new Date().toLocaleDateString();
+	const dateBlockInner = createElement('div', currentDay)
+	dateBlock.append(dateBlockInner);
+
+	//блоки гифок
+	const gifPath = 'gifs/' + gif + '_result.gif';
+	const firstGIF = createElement('div', '', {
+		'class' : isHidden + 'gif-block gif-first'});
+	const secondGIF = createElement('div', '', {
+		'class' : isHidden + 'gif-block gif-second'});
+	for (i=0; i<2; i++){
+		const img = createElement('img', '', {
+			'src' : gifPath
+		})
+		firstGIF.append(img);
+		const img2 = createElement('img', '', {
+			'src' : gifPath
+		})
+		secondGIF.append(img2);
+	}
+
+	//блок результата
+	const resultBlock = createElement('div', '', {
+		'class' : isHidden + 'result-block ' + color
+	})
+	const header = createElement('h1', job)
+	resultBlock.append(header);
+
+	//блок с хэдэром
+	const topBlock = createElement('div','',{
+		'class' : 'wrapper top-block'
+	})
+	topBlock.innerHTML = "<h2>" + text + "</h2>" + "<a href='#' class='link'>нажми чтобы узнать</a>"
+
+	//блок завтра
+	const tommorowBlock = createElement('div','',{
+		'class' : isHidden + 'wrapper bot-block'
+	})
+	tommorowBlock.innerHTML = "<a href='#' class='tommorow'><h2>А ЗАВТРА???</h2></a>"
+
+	//конструктор
+	const app = document.querySelector('#app');
+	app.append(dateBlock);
+	app.append(firstGIF);
+	app.append(topBlock);
+	app.append(resultBlock);
+	app.append(secondGIF);
+	app.append(tommorowBlock);
 }
 
 //функция для создания блоков
@@ -34,53 +88,3 @@ function createElement(tagName,content,attributes){
 	}
 	return newElement;
 }
-
-//блок с датой
-const dateBlock = createElement('div', '', {
-	'class' : 'wrapper date-block'
-})
-const currentDay = new Date().toLocaleDateString();
-const dateBlockInner = createElement('div', currentDay)
-dateBlock.append(dateBlockInner);
-
-//блоки гифок
-const gifPath = 'gifs/' + gif + '_result.gif';
-const firstGIF = createElement('div', '', {
-	'class' : 'hidden gif-block gif-first'});
-const secondGIF = createElement('div', '', {
-	'class' : 'hidden gif-block gif-second'});
-for (i=0; i<2; i++){
-	const img = createElement('img', '', {
-		'src' : gifPath
-	})
-	firstGIF.append(img);
-	const img2 = createElement('img', '', {
-		'src' : gifPath
-	})
-	secondGIF.append(img2);
-}
-
-//блок результата
-const resultBlock = createElement('div', '', {
-	'class' : 'hidden result-block ' + color
-})
-const header = createElement('h1', job)
-resultBlock.append(header);
-
-//конструктор
-const app = document.querySelector('#app');
-app.prepend(firstGIF);
-app.prepend(dateBlock);
-app.append(resultBlock);
-app.append(secondGIF);
-
-//Клик на текст
-const link = document.querySelector('.link');
-const result = document.querySelector('.result-block');
-
-link.addEventListener('click', ()=>{
-	link.classList.toggle('hidden');
-	result.classList.toggle('hidden');
-	firstGIF.classList.toggle('hidden');
-	secondGIF.classList.toggle('hidden');
-})
---
+++ click.js
@@ -0,0 +1,27 @@
+//Клик на текст
+const result = document.querySelector('.result-block');
+const tommorowBlock = document.querySelector('.bot-block');
+const firstGIF = document.querySelector('.gif-first');
+const secondGIF = document.querySelector('.gif-second');
+const link = document.querySelector('.link');
+
+link.addEventListener('click', ()=>{
+	link.classList.toggle('hidden');
+	tommorowBlock.classList.toggle('hidden');
+	result.classList.toggle('hidden');
+	firstGIF.classList.toggle('hidden');
+	secondGIF.classList.toggle('hidden');
+})
+
+const tommorow = document.querySelector('.tommorow');
+
+tommorow.addEventListener('click', ()=>{
+	app.innerHTML = '';
+	const [job, color, gif] = varMaker(workdays, -1);
+	buildApp(job, color, gif, false, 'А ЗАВТРА ХЕЛЬ...');
+	const link = document.querySelector('.link');
+	const tommorowBlock = document.querySelector('.bot-block');
+	tommorowBlock.classList.toggle('hidden');
+	link.classList.toggle('hidden');
+})
+
